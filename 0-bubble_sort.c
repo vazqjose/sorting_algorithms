b296456a4c36840f8d93a3890a7fe3b719b173fb
Jose Vazquez <vazqjose@gmail.com>
2021-06-14 21:29:32 +0000

    Task1

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,25 +1,38 @@
 #include "sort.h"
 
-
+/**
+ * swap - swap the given 2 items
+ * @xp: first item
+ * @yp: second item
+ * Return: Nothing
+ */
 void swap(int *xp, int *yp)
 {
 	int temp = *xp;
+
 	*xp = *yp;
 	*yp = temp;
 }
 
+/**
+ * bubble_sort - sort an array using the bubble sort algoritm
+ * @array: array to sort
+ * @size: size of array
+ * Return: Nothing
+ */
 void bubble_sort(int *array, size_t size)
 {
-	int i, j;
-	for (i = 0; i < n-1; i++)
+	size_t i, j;
+
+	for (i = 0; i < size - 1; i++)
 	{
-	// Last i elements are already in place  
-		for (j = 0; j < n-i-1; j++i)
+		for (j = 0; j < size - i - 1; j++)
 		{
-			if (array[j] > array[j+1])
+			if (array[j] > array[j + 1])
 			{
-				swap(&array[j], &array[j+1]);
-				print_array(array, size)
+				swap(&array[j], &array[j + 1]);
+
+				print_array(array, size);
 			}
 		}
 	}
--- sort.h
+++ sort.h
@@ -1,3 +1,6 @@
+#ifndef SORT_H
+#define SORT_H
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -10,7 +13,13 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *listi);
+void bubble_sort(int *array, size_t size);
+
+#endif
